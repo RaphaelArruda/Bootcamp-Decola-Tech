62f08920be31f71a497e53ae0d5fbff7889b50eb
Perkles <raphaelarruda.dev@gmail.com>
2021-12-06 00:13:41 -0300

    adc projeto unit test

---
+++ .net e c#/Ativnidade introduçaõ .net/ContasTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Tests
+{
+    [TestClass()]
+    public class ContasTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var contas = new Contas();
+
+            var result = contas.Soma(a, b);
+
+            Assert.AreEqual(,)
+
+
+        }
+    }
+}
